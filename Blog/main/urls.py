caad1fec3b23b842437cf0e01f5cd122ceb39e75
Nada Almutairi <nadaamusaad@gmail.com>
2023-08-28 11:21:29 +0300

    ..

--- Blog/main/urls.py
+++ Blog/main/urls.py
@@ -14,4 +14,3 @@ urlpatterns = [
     path("update/<post_id>/", views.post_update_view, name="post_update_view"),
     path("delete/<post_id>/", views.post_delete_view, name="post_delete_view"),
 ]
-]
